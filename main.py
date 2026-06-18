d07151f80d80159cc1e1c9f8e8d2d5e6d295efa1
Nicolas <ncomin@alumno.huergo.edu.ar>
2026-06-18 15:49:31 -0300

    empiezo con el jason

---
+++ main.py
@@ -0,0 +1,20 @@
+import json
+from typing import Dict, Set
+
+def mostrar_menu():
+    print("\n--- MENÚ PRINCIPAL ---")
+    print(" 1. Ver Pokédex")
+    print(" 2. Ver Equipo Principal")
+    print(" 3. Ver PC")
+    print(" 4. Capturar nuevo Pokémon")
+    print(" 5. Ordenar PC")
+    print(" 6. Buscar Pokémon en Equipo")
+    print(" 7. Consultar Pokémon en Pokédex por ID")
+    print(" 8. Enviar Pokémon al Centro Pokémon")
+    print(" 9. Transferir Pokémon al Profesor Oak")
+    print("10. Deshacer última transferencia")
+    print("11. Desafiar Líder de Gimnasio")
+    print("12. Ver Medallas")
+    print("13. Salir del sistema")
+
+def main
---
+++ pokemon.py
@@ -0,0 +1,9 @@
+class pokemon:
+    def __init__(self, id_pokemon: int, nombre: str, tipo: str, poder_combate: int):
+        self.id = id_pokemon
+        self.nombre = nombre
+        self.tipo = tipo
+        self.poder_combate = poder_combate
+
+    def __str__(self):
+        return 
